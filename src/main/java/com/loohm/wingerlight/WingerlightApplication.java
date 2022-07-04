556ae13be29e320680838fcace7afc60d952800c
LooHM25 <1063672411@qq.com>
2022-07-04 23:01:06 +0800

    init project

---
+++ src/main/java/com/loohm/wingerlight/WingerlightApplication.java
@@ -0,0 +1,13 @@
+package com.loohm.wingerlight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WingerlightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WingerlightApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/loohm/wingerlight/WingerlightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loohm.wingerlight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WingerlightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
